c813d43b51fd3fe325aa8c75853d296eaac8778d
kaju02525 <karunkumar02525@gmail.com>
2019-11-29 20:59:15 +0530

    winds spin wheel library

---
+++ winds_spin_wheel/src/main/java/com/winds/winds_spin_wheel/model/LuckyItem.java
@@ -0,0 +1,10 @@
+package com.winds.winds_spin_wheel.model;
+
+public class LuckyItem {
+    public String topText;
+    public String secondaryText;
+    public String secondaryText2;
+    public int secondaryTextOrientation;
+    public int icon;
+    public int color;
+}
